d73bff04a01b1858fe416fc2a49bcf4eb0e41284
Noma Dube <n.dube@saeny.net>
2015-06-16 11:59:33 -0400

    updated views pages with embedded ruby

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -4,25 +4,30 @@ class PagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | ThirstAlert"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About | ThirstAlert"
   end
 
   test "should get dudes" do
     get :dudes
     assert_response :success
+    assert_select "title", "Dudes | ThirstAlert"
   end
 
   test "should get chicks" do
     get :chicks
     assert_response :success
+    assert_select "title", "Chicks | ThirstAlert"
   end
 
   test "should get the_ex" do
     get :the_ex
     assert_response :success
+    assert_select "title", "The_Ex | ThirstAlert"
   end
 end
